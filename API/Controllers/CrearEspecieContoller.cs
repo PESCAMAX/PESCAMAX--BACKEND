c19896d79b1fd3ce6654b44e27b010fd5eb59130
Angelpdt <114520161+Angelpdt@users.noreply.github.com>
2024-06-18 00:59:51 -0500

    Update CrearEspecieContoller.cs
    cambio de int a float y cambio de nombres de campos

--- API/Controllers/CrearEspecieContoller.cs
+++ API/Controllers/CrearEspecieContoller.cs
@@ -1,15 +1,16 @@
-﻿using API.Modelo;
+using API.Modelo;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Data;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Cors;
 using TuProyecto.Models;
 
-namespace TuProyecto.Controllers
+namespace API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -24,8 +25,9 @@ namespace TuProyecto.Controllers
             _logger = logger;
         }
 
-        // POST: CrearEspecie/Crear
+        #region Crear especie
         [HttpPost]
+        [EnableCors("AllowSpecificOrigin")]
         [Route("Crear")]
         public async Task<IActionResult> Crear([FromBody] CrearEspecie crearEspecie)
         {
@@ -40,14 +42,14 @@ namespace TuProyecto.Controllers
                         {
                             CommandType = CommandType.StoredProcedure
                         };
-                        cmd.Parameters.AddWithValue("@Id", crearEspecie.Id);
+
                         cmd.Parameters.AddWithValue("@NombreEspecie", crearEspecie.NombreEspecie);
-                        cmd.Parameters.AddWithValue("@TdsSeguro", crearEspecie.TdsSeguro);
-                        cmd.Parameters.AddWithValue("@TdsPeligroso", crearEspecie.TdsPeligroso);
-                        cmd.Parameters.AddWithValue("@TemperaturaSeguro", crearEspecie.TemperaturaSeguro);
-                        cmd.Parameters.AddWithValue("@TemperaturaPeligroso", crearEspecie.TemperaturaPeligroso);
-                        cmd.Parameters.AddWithValue("@PhSeguro", crearEspecie.PhSeguro);
-                        cmd.Parameters.AddWithValue("@PhPeligroso", crearEspecie.PhPeligroso);
+                        cmd.Parameters.AddWithValue("@TdsMinimo", crearEspecie.TdsMinimo);
+                        cmd.Parameters.AddWithValue("@TdsMaximo", crearEspecie.TdsMaximo);
+                        cmd.Parameters.AddWithValue("@TemperaturaMinimo", crearEspecie.TemperaturaMinimo);
+                        cmd.Parameters.AddWithValue("@TemperaturaMaximo", crearEspecie.TemperaturaMaximo);
+                        cmd.Parameters.AddWithValue("@PhMinimo", crearEspecie.PhMinimo);
+                        cmd.Parameters.AddWithValue("@PhMaximo", crearEspecie.PhMaximo);
 
                         cmd.ExecuteNonQuery();
                         transaction.Commit();
@@ -62,9 +64,11 @@ namespace TuProyecto.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message });
             }
         }
+        #endregion
 
-        // POST: CrearEspecie/Modificar
-        [HttpPost]
+        #region Modificar especie
+        [HttpPut]
+        [EnableCors("AllowSpecificOrigin")]
         [Route("Modificar")]
         public async Task<IActionResult> Modificar([FromBody] CrearEspecie modificarEspecie)
         {
@@ -81,12 +85,12 @@ namespace TuProyecto.Controllers
                         };
                         cmd.Parameters.AddWithValue("@Id", modificarEspecie.Id);
                         cmd.Parameters.AddWithValue("@NombreEspecie", modificarEspecie.NombreEspecie);
-                        cmd.Parameters.AddWithValue("@TdsSeguro", modificarEspecie.TdsSeguro);
-                        cmd.Parameters.AddWithValue("@TdsPeligroso", modificarEspecie.TdsPeligroso);
-                        cmd.Parameters.AddWithValue("@TemperaturaSeguro", modificarEspecie.TemperaturaSeguro);
-                        cmd.Parameters.AddWithValue("@TemperaturaPeligroso", modificarEspecie.TemperaturaPeligroso);
-                        cmd.Parameters.AddWithValue("@PhSeguro", modificarEspecie.PhSeguro);
-                        cmd.Parameters.AddWithValue("@PhPeligroso", modificarEspecie.PhPeligroso);
+                        cmd.Parameters.AddWithValue("@TdsMinimo", modificarEspecie.TdsMinimo);
+                        cmd.Parameters.AddWithValue("@TdsMaximo", modificarEspecie.TdsMaximo);
+                        cmd.Parameters.AddWithValue("@TemperaturaMinimo", modificarEspecie.TemperaturaMinimo);
+                        cmd.Parameters.AddWithValue("@TemperaturaMaximo", modificarEspecie.TemperaturaMaximo);
+                        cmd.Parameters.AddWithValue("@PhMinimo", modificarEspecie.PhMinimo);
+                        cmd.Parameters.AddWithValue("@PhMaximo", modificarEspecie.PhMaximo);
 
                         cmd.ExecuteNonQuery();
                         transaction.Commit();
@@ -101,11 +105,13 @@ namespace TuProyecto.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message });
             }
         }
+        #endregion
 
-        // POST: CrearEspecie/Eliminar
-        [HttpPost]
-        [Route("Eliminar")]
-        public async Task<IActionResult> Eliminar([FromBody] int id)
+        #region Eliminar especie
+        [HttpDelete]
+        [EnableCors("AllowSpecificOrigin")]
+        [Route("Eliminar/{id}")]
+        public IActionResult Eliminar([FromRoute] int id)
         {
             try
             {
@@ -133,5 +139,54 @@ namespace TuProyecto.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message });
             }
         }
+        #endregion
+
+        #region Listar especie
+        [HttpGet]
+        [EnableCors("AllowSpecificOrigin")]
+        [Route("Listar")]
+        public async Task<IActionResult> Listar()
+        {
+            try
+            {
+                var especies = new List<CrearEspecie>();
+
+                using (var conexion = new SqlConnection(_cadenaSQL))
+                {
+                    conexion.Open();
+                    var cmd = new SqlCommand("sp_listar_especies", conexion)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var especie = new CrearEspecie
+                            {
+                                Id = reader.GetInt32(0),
+                                NombreEspecie = reader.GetString(1),
+                                TdsMinimo = (float)reader.GetDouble(2),
+                                TdsMaximo = (float)reader.GetDouble(3),
+                                TemperaturaMinimo = (float)reader.GetDouble(4),
+                                TemperaturaMaximo = (float)reader.GetDouble(5),
+                                PhMinimo = (float)reader.GetDouble(6),
+                                PhMaximo = (float)reader.GetDouble(7)
+                            };
+                            especies.Add(especie);
+                        }
+                    }
+                }
+
+                return Ok(especies);
+            }
+            catch (Exception error)
+            {
+                _logger.LogError($"Error en Listar: {error.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message });
+            }
+        }
+        #endregion
     }
 }
