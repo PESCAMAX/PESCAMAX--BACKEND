31ba865e1a0a95916f50a7f3cf914315726e9e7f
yulieth18x <141379700+yulieth18x@users.noreply.github.com>
2024-06-27 01:28:50 -0500

    actualizado

---
+++ Controllers/AlertaController.cs
@@ -0,0 +1,64 @@
+﻿using API.Data;
+using API.Modelo;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowSpecificOrigin")]
+    [ApiController]
+    public class AlertaController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AlertaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [EnableCors("AllowSpecificOrigin")]
+        public async Task<ActionResult<Alerta>> CrearAlerta(Alerta alerta)
+        {
+            try
+            {
+                var commandText = "EXEC spCrearAlerta @especieID, @Nombre, @loteID, @descripcion";
+                var parameters = new[]
+                {
+                    new SqlParameter("@especieID", alerta.EspecieID),
+                    new SqlParameter("@Nombre", alerta.Nombre),
+                    new SqlParameter("@loteID", alerta.LoteID),
+                    new SqlParameter("@descripcion", alerta.Descripcion)
+                };
+
+                await _context.Database.ExecuteSqlRawAsync(commandText, parameters);
+                return Ok(alerta);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [EnableCors("AllowSpecificOrigin")]
+        public async Task<ActionResult<IEnumerable<Alerta>>> ObtenerAlertas()
+        {
+            try
+            {
+                var alertas = await _context.Alertas.FromSqlRaw("EXEC spObtenerAlertas").ToListAsync();
+                return Ok(alertas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,11 +3,15 @@ using Microsoft.AspNetCore.Identity;
 using API.Modelo;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using API.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System;
+using System.Text;
+using API.Models;
+
 
 namespace API.Controllers
 {
@@ -17,21 +21,28 @@ namespace API.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IConfiguration _configuration;
+        private readonly IEmailService _emailService;
 
-        public AuthController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        public AuthController(UserManager<ApplicationUser> userManager, IConfiguration configuration, IEmailService emailService)
         {
             _userManager = userManager;
             _configuration = configuration;
+            _emailService = emailService;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
-            var user = new ApplicationUser { UserName = model.Username, Email = model.Email };
+            var user = new ApplicationUser { UserName = model.Username, Email = model.Email, EmailConfirmed = true }; // EmailConfirmed se establece en true
             var result = await _userManager.CreateAsync(user, model.Password);
 
             if (result.Succeeded)
             {
+                // Generar y almacenar el token
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                user.Token = token;
+                await _userManager.UpdateAsync(user);
+
                 return Ok(new { message = "User registered successfully" });
             }
 
@@ -46,9 +57,9 @@ namespace API.Controllers
             {
                 var claims = new[]
                 {
-            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-        };
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
 
                 var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
                 var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
@@ -75,5 +86,60 @@ namespace API.Controllers
             });
         }
 
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+                {
+                    // No revelar que el usuario no existe o no está confirmado
+                    return BadRequest(new { message = "El usuario no existe o no está confirmado." });
+                }
+
+                var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+                var callbackUrl = Url.Action("ResetPassword", "Auth", new { token, email = user.Email }, protocol: HttpContext.Request.Scheme);
+
+                var message = $"Por favor restablezca su contraseña haciendo clic en el siguiente enlace: <a href='{callbackUrl}'>link</a>";
+                await _emailService.SendEmailAsync(model.Email, "Restablecer contraseña", message);
+
+                return Ok(new { message = "Correo de restablecimiento de contraseña enviado." });
+            }
+
+            return BadRequest(new { message = "Solicitud inválida." });
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { message = "Solicitud inválida", errors = ModelState });
+            }
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Usuario no encontrado" });
+            }
+
+            var result = await _userManager.ResetPasswordAsync(user, model.Token, model.Password);
+            if (result.Succeeded)
+            {
+                return Ok(new { message = "Contraseña restablecida exitosamente" });
+            }
+
+            var errors = result.Errors.Select(e => e.Description).ToList();
+            return BadRequest(new { message = "Error al restablecer la contraseña", errors });
+        }
     }
 }
+
+
+
+
+
+
+
