64d0b5e3858e582fcd8c769cd5106cef4d6c89fd
Angelpdt <114520161+Angelpdt@users.noreply.github.com>
2024-09-03 00:06:58 -0500

    metodo de guardar y obtener  clima

---
+++ Controllers/ClimaController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using API.Modelo;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClimaController : ControllerBase
+    {
+        private readonly string _cadenaSQL;
+        private readonly ILogger<ClimaController> _logger;
+
+        public ClimaController(IConfiguration configuration, ILogger<ClimaController> logger)
+        {
+            _cadenaSQL = configuration.GetConnectionString("DefaultConnection");
+            _logger = logger;
+        }
+        [HttpPost]
+        public IActionResult GuardarClima([FromBody] Clima clima)
+        {
+            using (SqlConnection connection = new SqlConnection(_cadenaSQL))
+            {
+                SqlCommand command = new SqlCommand("InsertarClima", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@NombreCiudad", clima.NombreCiudad);
+                command.Parameters.AddWithValue("@TemperaturaActual", clima.TemperaturaActual);
+                command.Parameters.AddWithValue("@EstadoMeteoro", clima.EstadoMeteoro);
+                command.Parameters.AddWithValue("@Humedad", clima.Humedad);
+                command.Parameters.AddWithValue("@Nubes", clima.Nubes);
+                command.Parameters.AddWithValue("@FechaHora", DateTime.Now);
+                command.Parameters.AddWithValue("@UserId", clima.UserId);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("{userId}")]
+        public IActionResult ObtenerClima(string userId)
+        {
+            List<Clima> climas = new List<Clima>();
+
+            using (SqlConnection connection = new SqlConnection(_cadenaSQL))
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM Clima WHERE UserId = @UserId", connection);
+                command.Parameters.AddWithValue("@UserId", userId);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Clima clima = new Clima
+                    {
+                        NombreCiudad = reader["NombreCiudad"].ToString(),
+                        TemperaturaActual = Convert.ToDouble(reader["TemperaturaActual"]),
+                        EstadoMeteoro = reader["EstadoMeteoro"].ToString(),
+                        Humedad = Convert.ToInt32(reader["Humedad"]),
+                        Nubes = Convert.ToInt32(reader["Nubes"]),
+                        FechaHora = Convert.ToDateTime(reader["FechaHora"]),
+                        UserId = reader["UserId"].ToString()
+                    };
+                    climas.Add(clima);
+                }
+                connection.Close();
+            }
+
+            return Ok(climas);
+        }
+    }
+}
---
+++ Modelo/Clima.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Modelo
+{
+    public class Clima
+    {
+        public string NombreCiudad { get; set; }
+        public double TemperaturaActual { get; set; }
+        public string EstadoMeteoro { get; set; }
+        public int Humedad { get; set; }
+        public int Nubes { get; set; }
+        public DateTime FechaHora { get; set; }
+        public string UserId { get; set; }
+    }
+}
